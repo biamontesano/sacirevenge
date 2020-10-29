b425ddfa0b5645bacdb38d33b8ec2071166a1a90
Lucas Possam <lucaspossam@gmail.com>
2020-10-29 13:43:54 -0300

    add following camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody rb;
+    public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale;
+
+    private Vector3 moveDirection;
+
+   
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float mH = Input.GetAxis("Horizontal");
         float mV = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(mH * moveSpeed, rb.velocity.y, mV * moveSpeed);
+        moveDirection = new Vector3(mH * moveSpeed, moveDirection.y , mV * moveSpeed);
 
-    
+       
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+
+        controller.Move(moveDirection * Time.deltaTime);
 
 
     }
