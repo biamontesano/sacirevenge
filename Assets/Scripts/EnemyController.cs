40531ed7cfce3f3a9ddfe73075722b8829a3c79c
Bia Montesano <biamoreira_arq@yahoo.com.br>
2020-10-29 17:14:16 -0300

    EnemyController working w/ pursuit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float MoveSpeed = 2;
+    private Rigidbody Enemy;
+    public GameObject Player;
+    private Vector3 Direction;
+    private float Distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<Rigidbody>();
+        Player = GameObject.FindWithTag("Jogador");
+    }
+
+    private void FixedUpdate()
+    {
+        Direction = Player.transform.position - Enemy.transform.position;
+        Distance = Vector3.Distance(transform.position, Player.transform.position);
+
+
+        Quaternion NewRotation = Quaternion.LookRotation(Direction);
+        Enemy.MoveRotation(NewRotation);
+
+        if (Distance > 2 )
+         {
+            Movement(Direction, MoveSpeed);
+         }
+    }
+
+    public void Movement(Vector3 Move, float MS)
+    {
+        Enemy.MovePosition(Enemy.position + Move.normalized * MS * Time.deltaTime);
+    }
+
+}
