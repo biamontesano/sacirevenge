59e2e997c2f7dec3ac2f330bb735dc2801e73fb4
Anna Fernandes <anna.cbf@gmail.com>
2020-10-29 21:13:26 -0300

    Add lógica de npcs lokos do doce. Add obstáculo meramente ilustrativo, pra ver se desviavam.

---
+++ Assets/Scripts/NpcsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcsController : MonoBehaviour
+{
+    public Transform home;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(home.position);       
+    }
+
+}
