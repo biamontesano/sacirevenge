74d78774983128589b92c45c208be05df92f4ac6
Bia Montesano <biamoreira_arq@yahoo.com.br>
2020-11-01 12:14:42 -0300

    Tornado funcionando
    Saci funcionando
    Destroy funcionando (de itens e do saci)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,8 +16,10 @@ public class PlayerController : MonoBehaviour
     public int score;
     public GameObject Enemy;
     public bool PodeUsar;
-
     private Vector3 moveDirection;
+    public bool Atacando;
+    public float Distancia;
+    public bool sugar;
 
     public Animator anim;
 
@@ -33,6 +35,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //**********************Movimentação Personagem***************//
         float mH = Input.GetAxis("Horizontal");
         float mV = Input.GetAxis("Vertical");
        
@@ -49,8 +52,8 @@ public class PlayerController : MonoBehaviour
         }
 
         controller.Move(moveDirection * Time.deltaTime);
-        
-       
+        //**********************Movimentação Personagem***************//
+        //??
         anim.SetInteger("Score", score);
 
         // Skill Tornado
@@ -59,7 +62,7 @@ public class PlayerController : MonoBehaviour
             Physics.IgnoreCollision(controller, Enemy.GetComponent<Collider>(), true);
             SkillDuracao();
             PodeUsar = false;
-            StartCoroutine(SkillColdown(5f));
+            StartCoroutine(SkillColdown(5f)); //Precisa Configurar o Tempo.
         }
 
         IEnumerator SkillColdown(float coldown)
@@ -69,15 +72,19 @@ public class PlayerController : MonoBehaviour
             PodeUsar = true;
         }
 
+
+        //Animação de Ataque
         if (Input.GetKey(KeyCode.E))
         {
             anim.SetBool("Attack", true);
+            Atacando = true;
         } else
         {
             anim.SetBool("Attack", false);
+            Atacando = false;
         }
 
-        //Animação Movendo (A/S/W/D)
+        //Animação Movimentação (A/S/W/D)
         if (mH !=  0 || mV != 0)
         {
             anim.SetBool("Movendo", true);
@@ -86,6 +93,20 @@ public class PlayerController : MonoBehaviour
             anim.SetBool("Movendo", false);
         }
 
+
+        //SaciGarrafa - Puxa para Garrafa e Destroi o Jogador.
+        Distancia = Vector3.Distance(transform.position, Enemy.transform.position);
+
+        if (Distancia < 2 && PodeUsar == true )
+        {
+            sugar = true;
+            StartCoroutine(Sugando());
+        }
+        else
+        {
+            sugar = false;
+            anim.SetBool("Sugando?", false);
+        }
     }
 
 
@@ -93,13 +114,38 @@ public class PlayerController : MonoBehaviour
     public void SkillDuracao()
     {
         anim.SetBool("SkillTornado", true);
-        StartCoroutine(DuracaoSkill(4f));
+        StartCoroutine(DuracaoSkill(4f)); //Precisa Configurar o Tempo.
 
         IEnumerator DuracaoSkill(float duracao)
         {
             yield return new WaitForSeconds(duracao);
             PodeUsar = false;
             anim.SetBool("SkillTornado", false);
+        }
+    }
+
+    //Puxa para Garrafa e Destroi o Jogador.
+    public IEnumerator Sugando()
+    {
+        anim.SetBool("Sugando?", true);
+        yield return new WaitForSecondsRealtime(4); //Precisa Configurar o Tempo.
+        if(sugar == true)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            yield break;
+        }
+
+    }
+
+
+    //Destruindo Objetos.
+    private void OnTriggerEnter(Collider objetoDeColisao)
+    {
+        if (Atacando == true && objetoDeColisao.tag == "Destrutivel")
+        {
+            Destroy(objetoDeColisao.gameObject);
         }
     }
 
