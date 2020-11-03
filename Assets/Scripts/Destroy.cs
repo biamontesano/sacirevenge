5d741b701bce792989497ce91a80b737bf6faaba
Anna Fernandes <anna.cbf@gmail.com>
2020-11-03 00:05:34 -0300

    Ajustei distância do player para atirar nos itens.

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -7,17 +7,28 @@ public class Destroy : MonoBehaviour
     public int hits;
     public int points;
     public GameObject destroyedVersion;
+    public GameObject Player;
+    private float playerdistance;
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Jogador");
+    }
 
     private void OnMouseDown()
     {
-        hits--;
-        if(hits < 0)
-        {
-            Instantiate(destroyedVersion, transform.position, transform.rotation);
-            GameManager.Instance.Score += points;
-            Destroy(gameObject);
-        }
+        playerdistance = Vector3.Distance(transform.position, Player.transform.position);
 
+        if(playerdistance < 3)
+        {
+            hits--;
+            if(hits < 0)
+            {
+                Instantiate(destroyedVersion, transform.position, transform.rotation);
+                GameManager.Instance.Score += points;
+                Destroy(gameObject);
+            }
+        }
     }
 
 
